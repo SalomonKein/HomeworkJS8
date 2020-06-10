1cc1d83c5dbe099e9b4b8061d960533e9c101494
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2020-06-10 17:55:15 +0300

    add hw8

---
+++ script.js
@@ -0,0 +1,12 @@
+price.onclick = function(focusFild) {
+    if (focusFild.target.tagName != "price") return;
+
+    else {
+        let elem = document.getElementById('price');
+        elem.borderСolor = '#0af316';
+
+    }
+
+
+
+}
