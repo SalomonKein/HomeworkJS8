b10d16d0f0f5861a39f3a145ba3fa8c8c904032c
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2020-06-12 16:15:28 +0300

    add some changes

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function createNotification(){
-    let notification = document.createElement ('span')
-notification.className = "note"
+function createNotification(anchor, position, html) {
+    let notification = document.createElement('span');
+    notification.className = "note";
+    let curentPrice = document.querySelector('input');
+    notification.innerHTML = `Curent price: ${curentPrice.value}`;
+    price.before(notification);
 
-form.before('span');
+    positionAt(anchor, position, notification);
+}
+let form = document.querySelector('form')
+createNotification(form, "top", "note");
+
+function positionAt(anchor, position, elem) {
+
+    let anchorCoords = anchor.getBoundingClientRect();
+    switch (position) {
+        case "top":
+            elem.style.left = anchorCoords.left + "px";
+            elem.style.top = anchorCoords.top - 5 - elem.offsetHeight + "px";
+    }
+
 }
